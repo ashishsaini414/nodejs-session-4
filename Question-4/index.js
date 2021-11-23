426910f8e07bb21ac4e2b718d48f8e2991da6c8c
sainiashish948 <sainiashish948@gmail.com>
2021-11-23 23:04:26 +0530

    improvement

--- Question-4/index.js
+++ Question-4/index.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("ls-l",(error, stdout,stderr)=>{
+exec("ls -l",(error, stdout,stderr)=>{
     console.log(`All files in this directory = ${stdout}`)
     if(error){
         console.log("Command not found: " + error.message)
